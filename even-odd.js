66ffc9027c65ccae46035a251b252a1740df8d99
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-10-26 12:09:30 +0600

    create even-odd.js file for checking even or odd number using function

---
+++ even-odd.js
@@ -0,0 +1,15 @@
+// check even or odd number using function
+function isEven(num){
+    if(num % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+const number1 = isEven(98);
+console.log(number1);
+const number2 = isEven(117);
+console.log(number2);
