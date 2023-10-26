1cd5b032ac3438241fd54505a09332a135b77e80
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-10-26 10:44:25 +0600

    create converter.js file and convert inch into feet

---
+++ converter.js
@@ -0,0 +1,15 @@
+// convert inch tnto feet
+let standerdHeight = 7 ;
+let jimHeightInInch = 84 ;
+
+let jimHeight = jimHeightInInch / 12 ;
+console.log(jimHeight);
+
+if(jimHeight >= standerdHeight){
+    console.log("Jim can join the vollyball team");
+}
+else{
+    console.log("Jim can't join the vollyball team");
+}
+
+
